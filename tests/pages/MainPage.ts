bfd06345d9134d6d1e6c076a6ec70ade358394b7
Ivanov4D <nota72305@gmail.com>
2025-03-19 02:06:33 +0500

    main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -5,12 +5,37 @@ export class MainPage extends BasePage {
   private readonly headerLocator: Locator;
   private readonly categoriesTabsLocator: Locator;
   private readonly menuLocator: Locator;
+  private readonly headerAddButtonLocator: Locator;
+  private readonly headerNotificationsButtonLocator: Locator;
+  private readonly headerLoginButtonLocator: Locator;
+  private readonly headerAddButtonPopupListLocator: Locator;
+  private readonly headerNotificationsPopupLocator: Locator;
+  private readonly authModulLocator: Locator;
+  private readonly switchToRegModalLocator: Locator;
 
   constructor(page: Page) {
     super(page);
     this.headerLocator = this.page.locator('.header-module__charHeaderWrapper');
     this.categoriesTabsLocator = this.page.locator('.wdp-tabs-module__tabs');
     this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+    this.headerAddButtonLocator = this.page.getByRole('button', { name: 'Добавить' });
+    this.headerNotificationsButtonLocator = this.page.getByRole('button', { name: 'Уведомления' });
+    this.headerLoginButtonLocator = this.page.getByRole('button', { name: 'Вход и регистрация' });
+    this.headerAddButtonPopupListLocator = this.page.locator(
+      '.wdp-header-right-module__uploader ul',
+    );
+    this.headerNotificationsPopupLocator = this.page.locator(
+      '.wdp-notifications-popup-module__wrapper',
+    );
+    this.authModulLocator = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator('div[role="form"]');
+    this.switchToRegModalLocator = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator('div[role="form"]')
+      .getByRole('button', { name: 'Зарегистрироваться' });
   }
 
   async open() {
@@ -18,12 +43,46 @@ export class MainPage extends BasePage {
     await this.page.getByRole('button').filter({ hasText: /^$/ }).click();
   }
   async headerHasCorrectAriaSnapshot() {
-    await expect(this.headerLocator).toMatchAriaSnapshot();
+    await expect(this.headerLocator).toMatchAriaSnapshot({ name: 'headerAriaSnapShot.yml' });
   }
   async categoriesTabsHasCorrectAriaSnapshot() {
-    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot();
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot({
+      name: 'categoriesTabsAriaSnapshot.yml',
+    });
   }
   async menuHasCorrectAriaSnapshot() {
-    await expect(this.menuLocator).toMatchAriaSnapshot();
+    await expect(this.menuLocator).toMatchAriaSnapshot({ name: 'menuAriaSnapshot.yml' });
+  }
+  async openAddPopupList() {
+    await this.headerAddButtonLocator.click();
+  }
+  async openNotificationsPopup() {
+    await this.headerNotificationsButtonLocator.click();
+  }
+  async openAuthModal() {
+    await this.headerLoginButtonLocator.click();
+  }
+  async switchToRegModal() {
+    await this.switchToRegModalLocator.click();
+  }
+  async addPopupListHasCorrectAriaSnapshot() {
+    await expect(this.headerAddButtonPopupListLocator).toMatchAriaSnapshot({
+      name: 'addButtonList.yml',
+    });
+  }
+  async notificationsPopupListHasCorrectAriaSnapshot() {
+    await expect(this.headerNotificationsPopupLocator).toMatchAriaSnapshot({
+      name: 'notificationsPopup .yml',
+    });
+  }
+  async authModalHasCorrectAriaSnapshot() {
+    await expect(this.authModulLocator).toMatchAriaSnapshot({
+      name: 'authModal.yml',
+    });
+  }
+  async regModalHasCorrectAriaSnapshot() {
+    await expect(this.authModulLocator).toMatchAriaSnapshot({
+      name: 'regModal.yml',
+    });
   }
 }
--- tests/specs/unauth/mainPage.spec.ts
+++ tests/specs/unauth/mainPage.spec.ts
@@ -12,3 +12,24 @@ test('Проверка доступности элементов табов ка
 test('Проверка доступности элементов бокового меню слева', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
+
+test('Проверка доступности элементов списка добавления контента', async ({ mainPage }) => {
+  await mainPage.openAddPopupList();
+  await mainPage.addPopupListHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов попапа уведомлений', async ({ mainPage }) => {
+  await mainPage.openNotificationsPopup();
+  await mainPage.notificationsPopupListHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов модального окна авторизации', async ({ mainPage }) => {
+  await mainPage.openAuthModal();
+  await mainPage.authModalHasCorrectAriaSnapshot();
+});
+
+test('Проверка доступности элементов модального окна регистрации', async ({ mainPage }) => {
+  await mainPage.openAuthModal();
+  await mainPage.switchToRegModal();
+  await mainPage.regModalHasCorrectAriaSnapshot();
+});
